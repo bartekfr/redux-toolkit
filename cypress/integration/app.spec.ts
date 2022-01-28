6ff6a05411a2d6f1164d45806aba644662807c5a
Bartek Fryzowicz <bartekfr@wp.pl>
2022-01-28 19:25:41 +0100

    Add more tests

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,26 +1,40 @@
-// TODO move to fixtures
-
-const todos = [{
-  id: 1,
-  title: "Buy eggs"
-}, {
-  id: 2,
-  title: "Go for a walk"
-}]
-
 describe('App test', () => {
-  it('Renders correct elements', () => {
-    cy.visit('/')
-    cy.get('h2')
-
+  it('Renders loaded elements', () => {
     cy.intercept(
       {
         method: 'GET',
         url: 'https://jsonplaceholder.typicode.com/todos'
-      },
-      [...todos]
+      }, {
+        fixture: 'todos.json'
+      }
     ).as('getTodos')
 
-    cy.get('.todoList .todo').should('have.length', 2)
+    cy.visit('/')
+    cy.get('h2')
+
+    cy.wait('@getTodos')
+
+    cy.get('.todoList .todo')
+      .should('have.length', 2)
+      .each($el => {
+        cy.wrap($el).should('not.have.class', 'completeTodo')
+      })
+  })
+
+  it('Adds todo', () => {
+    cy.get('#todo-input').type('Shopping{enter}')
+
+    cy.get('.todoList .todo')
+      .should('have.length', 3)
+      .last()
+      .contains('Shopping')
+  })
+
+  it('Completes todo', () => {
+    cy.get('.todoList .todo:last-child').as('newItem')
+      .should('not.have.class', 'completeTodo')
+
+    cy.get('@newItem').find('.todoCheck').click()
+    cy.get('@newItem').should('have.class', 'completeTodo')
   })
 })
